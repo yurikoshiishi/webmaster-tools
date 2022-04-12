25d488dc633e7f6865cb7f56dd6b3b788c34a615
Yuri Koshiishi <55642841+yurikoshiishi@users.noreply.github.com>
2022-04-12 11:35:35 +0900

    modify: colorScheme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,15 +1,20 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, withDefaultColorScheme } from "@chakra-ui/react";
 
-export const theme = extendTheme({
-  styles: {
-    global: {
-      "*": {
-        fontFamily: `'ui-sans-serif','system-ui','-apple-system','BlinkMacSystemFont','Helvetica Neue','Arial','Hiragino Kaku Gothic ProN','Hiragino Sans','Meiryo','sans-serif'`,
-      },
-      "html, body, #__next": {
-        height: "100%",
-        width: "100%",
+export const theme = extendTheme(
+  withDefaultColorScheme({
+    colorScheme: "messenger",
+  }),
+  {
+    styles: {
+      global: {
+        "*": {
+          fontFamily: `'ui-sans-serif','system-ui','-apple-system','BlinkMacSystemFont','Helvetica Neue','Arial','Hiragino Kaku Gothic ProN','Hiragino Sans','Meiryo','sans-serif'`,
+        },
+        "html, body, #__next": {
+          height: "100%",
+          width: "100%",
+        },
       },
     },
-  },
-});
+  }
+);
